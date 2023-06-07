6142d17602e1084b0ab438ce1c0024833da218cf
Ryan Araujo <ryanaraujo01112001@gmail.com>
2023-06-07 14:55:23 -0300

    gerador de senhas aleatorias

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let sliderElement = document.querySelector("#slider");
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ123456789@!_";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
+
+function gerarSenha() {
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; i++) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copy() {
+  alert("senha copiada com sucesso!");
+  navigator.clipboard.writeText(novaSenha);
+}
